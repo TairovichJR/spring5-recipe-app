ca42f2c2df4d0b65b2fdb768ae6bd3a518218dbe
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-10 15:13:02 -0400

    adding difficulty

--- src/main/java/guru/springframework/recipe/domain/Recipe.java
+++ src/main/java/guru/springframework/recipe/domain/Recipe.java
@@ -4,6 +4,8 @@ import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -31,6 +33,9 @@ public class Recipe {
 	
 	@Lob
 	private Byte[] image;
+	
+	@Enumerated(value = EnumType.STRING)
+	private Difficulty difficulty;
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
@@ -101,8 +106,12 @@ public class Recipe {
 	public void setIngredients(Set<Ingredient> ingredients) {
 		this.ingredients = ingredients;
 	}
+	public Difficulty getDifficulty() {
+		return difficulty;
+	}
+	public void setDifficulty(Difficulty difficulty) {
+		this.difficulty = difficulty;
+	}
 	
-	
-	
-	
+
 }

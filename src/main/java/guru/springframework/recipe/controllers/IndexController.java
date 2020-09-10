dd5f8a72da50246b7a017e76265cb8b1226374e1
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-10 00:43:17 -0400

    adding pojos

---
+++ src/main/java/guru/springframework/recipe/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	
+	@RequestMapping({"","/","/index"})
+	public String getIndexPage() {
+		System.out.println("hello world console......");
+		return "index";
+	}
+	
+	
+	
+}

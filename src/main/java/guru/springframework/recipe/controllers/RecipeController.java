8212b98a141616a32225ab89faa498ecb51afb64
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-24 22:58:39 -0400

    added recipe page

--- src/main/java/guru/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipe/controllers/RecipeController.java
@@ -2,9 +2,13 @@ package guru.springframework.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.recipe.commands.RecipeCommand;
 import guru.springframework.recipe.services.RecipeService;
 
 /**
@@ -25,5 +29,26 @@ public class RecipeController {
 		model.addAttribute("recipe", recipeService.findById(Long.parseLong(id)));
 		return "/recipe/show";
 	}
+	
+	
+	@RequestMapping("/recipe/new")
+	public String getNewRecipe(Model model) {
+		
+		model.addAttribute("recipe", new RecipeCommand());
+		
+		return "recipe/recipeform";
+	}
+	
+	
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+		
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+		
+		return "redirect:/recipe/show/" + savedCommand.getId(); 
+	}
+	
+	
 	
 }

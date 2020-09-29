4cbe91b40eee1200544d21470d5466abce9fa28a
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-29 00:16:48 -0400

    Added saveImage tests

--- src/main/java/guru/springframework/recipe/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/recipe/services/ImageServiceImpl.java
@@ -1,8 +1,12 @@
 package guru.springframework.recipe.services;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import guru.springframework.recipe.domain.Recipe;
+import guru.springframework.recipe.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -12,12 +16,45 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class ImageServiceImpl implements ImageService{
 
+	private final RecipeRepository recipeRepository;
 	
-	
+	public ImageServiceImpl(RecipeRepository recipeRepository) {
+		this.recipeRepository = recipeRepository;
+	}
+
 	@Override
+	@Transactional
 	public void saveImageFile(Long recipeId, MultipartFile file) {
 		
-		log.debug("Recieved a file");
+		try {
+			Recipe recipe = recipeRepository.findById(recipeId).get();
+			Byte[] byteObjects = new Byte[file.getBytes().length];
+			int i = 0;
+			
+			for (Byte b : file.getBytes()) {
+				byteObjects[i++] = b;
+			}
+			
+			recipe.setImage(byteObjects);
+			recipeRepository.save(recipe);
+			
+		} catch (Exception e) {
+			//TODO handle better
+			log.error("Error Occurred ", e);
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+

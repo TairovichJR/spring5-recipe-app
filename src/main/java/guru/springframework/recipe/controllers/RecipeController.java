55f0d2949527deb8e3ebe465f856f3a89a53ba9c
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-10-01 18:07:31 -0400

    Adding custom error messages

--- src/main/java/guru/springframework/recipe/controllers/RecipeController.java
+++ src/main/java/guru/springframework/recipe/controllers/RecipeController.java
@@ -1,8 +1,11 @@
 package guru.springframework.recipe.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -25,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class RecipeController {
 
+	private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 	private final RecipeService recipeService;
 
 	public RecipeController(RecipeService recipeService) {
@@ -51,31 +55,40 @@ public class RecipeController {
 	}
 
 	@PostMapping("recipe")
-	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+	public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+
+		if (bindingResult.hasErrors()) {
+
+			bindingResult.getAllErrors().forEach(objectError -> {
+				log.debug(objectError.toString());
+			});
+
+			return RECIPE_RECIPEFORM_URL;
+		}
+
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
 	}
 
-    @RequestMapping("recipe/{id}/delete")
-    public String deleteById(@PathVariable String id){
+	@RequestMapping("recipe/{id}/delete")
+	public String deleteById(@PathVariable String id) {
 
-        log.debug("Deleting id: " + id);
+		log.debug("Deleting id: " + id);
 
-        recipeService.deleteById(Long.valueOf(id));
-        return "redirect:/";
-    }
-	
-    
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception) {
-    	log.error("Handling not found exception");
-    	log.error(exception.getMessage());
-    	ModelAndView modelAndView = new ModelAndView();
-    	modelAndView.setViewName("404error");
-    	modelAndView.addObject("exception", exception);
-    	return modelAndView;
-    }
-    
+		recipeService.deleteById(Long.valueOf(id));
+		return "redirect:/";
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound(Exception exception) {
+		log.error("Handling not found exception");
+		log.error(exception.getMessage());
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("404error");
+		modelAndView.addObject("exception", exception);
+		return modelAndView;
+	}
+
 }
--- src/test/java/guru/springframework/recipe/controller/RecipeControllerTest.java
+++ src/test/java/guru/springframework/recipe/controller/RecipeControllerTest.java
@@ -95,12 +95,32 @@ public class RecipeControllerTest {
 
         mockMvc.perform(post("/recipe")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .param("id", "")
-//                .param("description", "some string")
+                .param("id", "")
+                .param("description", "some string")
+                .param("directions", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
+	
+	@Test
+	public void testPostNewRecipeFormValidationFail() throws Exception{
+		RecipeCommand command = new RecipeCommand();
+		command.setId(2L);
+		
+		when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+		
+		mockMvc.perform(post("/recipe")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+				.param("id", "")
+				.param("cookTime", "3000")
+				)
+				.andExpect(status().isOk())
+				.andExpect(view().name("recipe/recipeform"))
+				.andExpect(model().attributeExists("recipe"));
+			
+	}
+	
 	
 	@Test
     public void testGetUpdateView() throws Exception {
